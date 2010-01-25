fe63af6130973fc0f18ecc78d3548898b63e154d
Jesper André Lyngesen Pedersen <jalp@nonutshell.com>
2010-01-25 15:41:20 +0100

    First simple message transfer

--- scripts/init.lua
+++ scripts/init.lua
@@ -2,7 +2,7 @@ require ('blocks')
 
 print("Starting up...");
 
-p = blocks.spawn(function() print 'hi there from a different thread of execution' return function(a) print (a) end end)
+p = blocks.spawn(function() print 'hi there from a different thread of execution' return function(a,b,c) print (a .. ', ' .. b .. ', ' .. c) end end)
 
 r = p:send(10, 1.223, 'Hello there', {fisk = 2}, function(a) a() end)
 --print (r:get())
--- src/blocks/lua_message.c
+++ src/blocks/lua_message.c
@@ -6,13 +6,18 @@
  */
 #include <stdlib.h>
 #include <lauxlib.h>
+#include <string.h>
 #include "lua_message.h"
 #include <util/log.h>
 #include <util/lua_util.h>
 
+#ifndef MSG_MAX_SIZE
+#define MSG_MAX_SIZE 512
+#endif
+
 struct value {
 	short type;
-	int length;
+	size_t length;
 	void *data;
 };
 
@@ -47,7 +52,7 @@ int lua_message_push(lua_State *L, message_content_t *content) {
 		case LUA_TNONE:
 			break;
 		case LUA_TSTRING:
-			lua_pushstring(L, "fisk");
+			lua_pushlstring(L, arg->data, arg->length);
 			break;
 		default:
 			log_debug("type: %s", lua_typename(L, arg->type));
@@ -67,8 +72,9 @@ static void *get_index(void *heap, int size) {
 
 message_content_t *lua_message_pop(lua_State *L) {
 	int argc, i, type;
+	size_t length, total_length;
 	int message_size;
-	void *raw_message;
+	void *content_heap;
 	message_content_t *content;
 	struct values *args;
 	struct value *arg;
@@ -78,19 +84,31 @@ message_content_t *lua_message_pop(lua_State *L) {
 	message_size = sizeof(message_content_t) + sizeof(struct values)
 			+ (sizeof(struct value) * argc);
 
-	raw_message = malloc(message_size);
+	content = malloc(message_size + MSG_MAX_SIZE);
+	content_heap = get_index(content, message_size);
+	total_length = 0;
+	args = get_index(content, sizeof(message_content_t));
 
-	content = raw_message;
-	args = get_index(raw_message, sizeof(message_content_t));
 	content->message = args;
 	args->size = argc;
 	arg = get_index(args, sizeof(struct values));
 	args->values = arg;
 	for (i = 2; i <= argc+1; i++) {
 		type = lua_type(L, i);
-		arg->type = type;
-		log_debug("Type: %s. Size: %d", lua_typename(L, type), lua_objlen(L, i));
+		arg->type = LUA_TSTRING;
+		length = lua_objlen(L, i);
+		total_length += length;
+		if (total_length > MSG_MAX_SIZE) {
+			log_error("Max size of %d exceeded by %d", MSG_MAX_SIZE, total_length - MSG_MAX_SIZE);
+			free(content);
+			return NULL;
+		}
+		arg->length = length;
+		arg->data = content_heap;
+		memcpy(arg->data, lua_tolstring(L, i, &length), length);
+		log_debug("Type: %s. Size: %d", lua_typename(L, type), length);
 		arg = arg + 1;
+		content_heap = get_index(content_heap, length);
 	}
 
 	log_debug("arguments: %d, %p (%p)", argc, content->message, (void*)args->values);
