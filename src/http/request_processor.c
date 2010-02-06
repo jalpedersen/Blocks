4dcbb233d50e5f95f6f0170136b42138bd219838
Jesper André Lyngesen Pedersen <jalp@nonutshell.com>
2010-02-06 16:39:36 +0100

    mime-types for static pages

--- src/http/request_processor.c
+++ src/http/request_processor.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <unistd.h>
 #include <lua.h>
 #include <fcntl.h>
@@ -27,7 +28,9 @@ struct http_conn_info {
 	int client_sd;
 	int tmp_file_sd;
 	lua_State *L;
+	char type;
 };
+
 
 #define save_data(_conn_info, data_ptr, data_size, new_data_ptr, new_data_size) \
 		struct http_conn_info *conn_info; \
@@ -40,21 +43,106 @@ struct http_conn_info {
 		conn_info->data_ptr = str; \
 		conn_info->data_size = new_size; \
 
-static const char *header = "HTTP/1.1 200 OK\nConnection: Close\nContent-Type: text/html\r\n\n";
+struct mime_type {
+	const char *postfix;
+	int postfix_size;
+	const char *mimetype;
+	int mimetype_size;
 
-static int start_processing(http_parser *parser) {
+};
+static struct mime_type json_mimetype = {"json", 4, "application/json", 16};
+static struct mime_type bin_mimetype = {"bin", 3, "application/octet-stream", 24};
+
+static struct mime_type mimetypes[] = {
+		{"html", 4, "text/html", 9},
+		{"htm", 3, "text/html", 9},
+		{"png", 3, "image/png", 9},
+		{"css", 3, "text/css", 8},
+		{"js", 2, "text/javascript", 15},
+		{"txt", 3, "text/plain", 10},
+		NULL
+};
+
+static int send_header(int sd, int status, struct mime_type *type) {
+	int sent;
+	char *status_str;
+	switch (status) {
+	case 200: status_str = "200 OK"; break;
+	case 402: status_str = "403 FORBIDDEN"; break;
+	case 404: status_str = "404 NOT FOUND"; break;
+	default: status_str = "500 INTERNAL ERROR";
+	}
+	sent = send(sd, "HTTP/1.1 ", 9, 0);
+	sent += send(sd, status_str, strlen(status_str), 0);
+	sent += send(sd, "\nConnection: Close\nContent-Type: ", 33, 0);
+	sent += send(sd, type->mimetype, type->mimetype_size, 0);
+	sent += send(sd, "\r\n\n", 3, 0);
+	return sent;
+}
+
+static int http_lua_eval(http_parser *parser) {
 	struct http_conn_info *conn_info;
 	lua_State *L;
 	conn_info = (struct http_conn_info*)parser->data;
+
 	/* Send HTTP headers to client */
-	send(conn_info->client_sd, header, strlen(header), 0);
+	send_header(conn_info->client_sd, 200, &json_mimetype);
 	L = conn_info->L;
 	lua_settop(L, 0);
 	lua_getglobal(L, "dispatch");
 	lua_pushstring(L, conn_info->path);
 	lua_pushstring(L, conn_info->query);
 	lua_pushinteger(L, conn_info->client_sd);
+	switch(parser->method) {
+	case HTTP_GET: lua_pushstring(L, "GET");break;
+	case HTTP_POST: lua_pushstring(L, "POST");break;
+	default: lua_pushstring(L, "UNKNOWN");
+	}
 	lua_eval(L);
+	return 0;
+}
+
+static struct mime_type *get_mimetype(const char *path) {
+	char *postfix;
+	struct mime_type *t;
+	postfix = rindex(path, '.');
+	if (postfix != NULL) {
+		postfix += 1;
+		t = mimetypes;
+		while (t->postfix != NULL) {
+			if (strncasecmp(t->postfix, postfix, t->postfix_size) == 0) {
+				return t;
+			}
+			t += 1;
+		}
+	}
+	return &bin_mimetype;
+}
+
+static int send_file(http_parser *parser) {
+	struct http_conn_info *conn_info;
+
+	conn_info = (struct http_conn_info*)parser->data;
+	const char *dummy = "<html><h1>Kashmir</h1></html>";
+	send_header(conn_info->client_sd, 200, get_mimetype(conn_info->path));
+
+	send(conn_info->client_sd, dummy, strlen(dummy), 0);
+
+	return 0;
+}
+
+static int start_processing(http_parser *parser) {
+	struct http_conn_info *conn_info;
+	conn_info = (struct http_conn_info*)parser->data;
+
+	if (strncmp("/l/", conn_info->path, 3) == 0) {
+		conn_info->type = 'L';
+		http_lua_eval(parser);
+	} else {
+		log_debug("path : %s", conn_info->path);
+		conn_info->type = 'S';
+		send_file(parser);
+	}
 	conn_info->tmp_file_sd = open("tmp-data", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 	if (conn_info->tmp_file_sd < 0) {
 		log_perror("While opening file for client: %d", conn_info->client_sd);
@@ -66,6 +154,9 @@ static int message_complete(http_parser *parser) {
 	struct http_conn_info *conn_info;
 	conn_info = (struct http_conn_info*)parser->data;
 	close(conn_info->tmp_file_sd);
+	if (conn_info->type != 'L') {
+		close(conn_info->client_sd);
+	}
 	log_debug("Closed file %d", conn_info->tmp_file_sd);
 	log_debug("Message complete: %d", conn_info->client_sd);
 	return 0;
@@ -118,8 +209,6 @@ http_parser *request_processor_reset(http_parser *parser, int client_sd, lua_Sta
 	parser->on_header_field = NULL;
 	parser->on_header_value = NULL;
 	parser->on_path = on_path;
-	//parser->on_url = print_data;
-	parser->on_fragment = NULL;
 	parser->on_query_string = on_query;
 	parser->on_body = on_body;
 	parser->on_headers_complete = start_processing;
